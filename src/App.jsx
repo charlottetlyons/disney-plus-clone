f2fcb7f78ea2f75ee58e5af480c6c6ea17699714
Charlotte Lyons <charlottetlyons@gmail.com>
2024-04-09 00:06:26 -0500

    Routing and Styled Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/Home";
 
 const App = () => {
+    const router = createBrowserRouter([
+        {
+            path: "/",
+            element: <Home/>
+        },
+        {
+            path: "/about",
+            element: <h1>About</h1>
+        }
+    ]);
+
     return (
-        <div>
-            <h1>Disney+</h1>
-            <a href="/">Click Here!</a>
-        </div>
+        <RouterProvider router={router}/>
     );
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -43,7 +43,7 @@ module.exports = {
         }),
     ],
     resolve: {
-        extensions: ['.js'],
+        extensions: ['.js', '.jsx'],
     },
     devServer: {
         static: path.resolve(__dirname, 'dist'),
